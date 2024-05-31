a14d470aefe273735432b60b770309d70e3b0ee0
igorbrasileiro <brasileiro456@gmail.com>
2024-05-31 11:02:29 -0300

    Create schemas

--- db/schema.ts
+++ db/schema.ts
@@ -1,14 +1,8 @@
-
-/**
-* The code snippet below is an example.
-*/
-
-/**
-* import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const profiles = sqliteTable("profiles", {
   id: integer("id").primaryKey({ autoIncrement: true }),
-  name: text("name"),
+  name: text("name").notNull(),
   email: text("email"),
+  phone: text("phone"),
 });
-*/
